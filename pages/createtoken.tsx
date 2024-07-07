67a0fa5cd034117147fe5ef96e78c5698c413bee
evan wang <evanwang@evans-MacBook-Pro.local>
2024-07-07 09:31:34 -0400

    toggle wallet info working

--- pages/createtoken.tsx
+++ pages/createtoken.tsx
@@ -1,30 +1,10 @@
 import { useState } from 'react';
 import { NextPage } from 'next';
 
-import main from '../utilities/main';
+import WalletContextProvider from '../components/WalletContextProvider'
 import { AppBar } from '../components/AppBar'
 import  CreateWallet from '../components/CreateWallet'
 import { useWallet } from '../components/useWalletContext';
-
-
-const {
-    Connection,
-    Keypair,
-} = require('@solana/web3.js');
-
-
-
-
-
-
-const endpoint = "https://mainnet.helius-rpc.com/?api-key=a78d80e6-ce3a-4c9f-81a1-d6dedf6e70c2";
-const NFT_STORAGE_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI1NzM2MmYzNi0wOWUyLTQzOGUtODgzNi01NWJkM2YxMjAzNWEiLCJlbWFpbCI6ImV2YW53YW5nMjAwMkBwcm90b24ubWUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX0seyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiZWJmZDQyMTgyYTI5ZDM0ZjhiMjkiLCJzY29wZWRLZXlTZWNyZXQiOiI3MzA0YWMyZDRlMTE4YTkyN2ZiN2RjOTA3NDA3MzJmMWNlYzQ0ODEyYTMyZDcyYjE0NzU3MDQ1ZjY5N2IyNGNkIiwiaWF0IjoxNzE5Njk1Nzk3fQ.FbQ0bO2bPSEAOx-BTg2g0SBRpMsu7N99DZLIxUwPYck';
-
-const revokeMintBool = true
-const revokeFreezeBool  = true
-
-
-
 
 const Createtoken: NextPage = () => {
 
@@ -41,45 +21,7 @@ const Createtoken: NextPage = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-  const connection = new Connection(endpoint, 'confirmed');
-    
-
-  // Get form data
-  const amount = event.target.elements.amount.value;
-  const decimals = event.target.elements.decimals.value;
-  const metadata = event.target.elements.metadata.value;
-  const symbol = event.target.elements.symbol.value;
-  const tokenName = event.target.elements.tokenName.value;
-  const description = event.target.elements.description.value;
-
-  // Store info in a variable
-  let tokenInfo = {
-    amount: amount,
-    decimals: decimals,
-    metadata: metadata,
-    symbol: symbol,
-    tokenName: tokenName
-  };
-
-  let metaDataforToken = {
-    "name": tokenInfo.tokenName,
-    "symbol": tokenInfo.symbol,
-    "image": '',
-    "description": description
-  };
-
-  try {
-    main(NFT_STORAGE_TOKEN, revokeMintBool, revokeFreezeBool, tokenInfo, metaDataforToken, connection, wallet);
-
-} catch (error) {
-    console.error(error);
-    return
-}
-
-
-
-
+    // Handle form submission here
   };
 
   const handleShowWallet = () => {
@@ -102,7 +44,7 @@ const Createtoken: NextPage = () => {
     {showWallet && wallet && (
         <div>
           <h2>Wallet Info</h2>
-          <p>Public Key: {wallet}</p>
+          <p>Public Key: {wallet.publicKey.toString()}</p>
           {/* Don't display private key in a real application */}
           <p>Private Key: {wallet.secretKey.toString()}</p>
         </div>
